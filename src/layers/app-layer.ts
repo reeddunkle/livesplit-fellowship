bc77440952d9990c06203a5f820da1d1eb360a38
Reed Dunkle <11543198+reeddunkle@users.noreply.github.com>
2026-08-14 11:37:50 -0400

    remove unused export

--- src/layers/app-layer.ts
+++ src/layers/app-layer.ts
@@ -7,10 +7,7 @@ import { FellowshipLive } from "@/services/fellowship/fellowship-service.ts";
 import { FileMonitorLive } from "@/services/filesystem/file-monitor-service.ts";
 import { LiveSplitFileLive } from "@/services/live-split/files/live-split-file-service.ts";
 
-export const PlatformLive = Layer.mergeAll(
-  NodeFileSystem.layer,
-  NodePath.layer,
-);
+const PlatformLive = Layer.mergeAll(NodeFileSystem.layer, NodePath.layer);
 
 const AppLoggerWithPlatform = AppLoggerLive.pipe(Layer.provide(PlatformLive));
 
