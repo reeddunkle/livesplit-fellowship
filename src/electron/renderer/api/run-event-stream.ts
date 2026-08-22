15c55275542ee60fe1dc7d10121614b043675d11
Reed Dunkle <11543198+reeddunkle@users.noreply.github.com>
2026-08-22 10:00:04 -0400

    unused export

--- src/electron/renderer/api/run-event-stream.ts
+++ src/electron/renderer/api/run-event-stream.ts
@@ -19,7 +19,7 @@ export const API_CONNECTION_STATE = {
   DISCONNECTED: "DISCONNECTED",
 } as const;
 
-export type ApiConnectionState =
+type ApiConnectionState =
   (typeof API_CONNECTION_STATE)[keyof typeof API_CONNECTION_STATE];
 
 export type ApiClientEvent =
